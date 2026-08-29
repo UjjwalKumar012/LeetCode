ff8003410da9aec2822b4837551707f7e3ba2032
Ujjwal Kumar <satyamujjwal2003@gmail.com>
2026-08-29 22:44:48 +0530

    Time: 19 ms (16.32%), Space: 50 MB (25.66%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+            for (int c : count) {
+                key.append('#').append(c);
+            }
+
+            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
